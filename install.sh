8871f368368170d85929de6a20dc3baa0f6a76fa
mhu <marcel.huismann@protonmail.ch>
2023-12-23 14:35:36 -0500

    switched from Nix to APT + Flatpak

--- install.sh
+++ install.sh
@@ -1,42 +1,22 @@
-#!/bin/bash
+sudo apt update
 
-# package manager
-sh <(curl -L https://nixos.org/nix/install) --daemon
+# Install Flatpak and add the Flathub repo
+sudo apt install flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-NIXPKGS_ALLOW_UNFREE=1 nix-env -iA nixpkgs.android-studio \
-    nixpkgs.curl \
-    nixpkgs.discord \
-    nixpkgs.docker \
-    nixpkgs.exa \
-    nixpkgs.feh \
-    nixpkgs.firefox \
-    nixpkgs.fzf \
-    nixpkgs.git \
-    nixpkgs.hugo \
-    nixpkgs.i3 \
-    nixpkgs.nettools \
-    nixpkgs.node \
-    nixpkgs.obsidian \
-    nixpkgs.parallel \
-    nixpkgs.python3 \
-    nixpkgs.signal-desktop \
-    nixpkgs.spotify \
-    nixpkgs.vscode \
-    nixpkgs.zoom-us \
-    nixpkgs.zsh
+# Install applications
+sudo apt install -y curl exa feh fzf git hugo i3 inkscape parallel zsh
+flatpak install -y flathub com.visualstudio.code com.discordapp.Discord org.signal.Signal com.spotify.Client md.obsidian.Obsidian
 
-mkdir ~/code
-
-# dotfiles
-git clone https://github.com/mhu/dotfiles.git ~/code/dotfiles
-
-# zsh
+# Set up zsh
 chsh -s /bin/zsh
 ln -s ~/code/dotfiles/.zshrc ~/.zshrc
 
-# link config files
-mkdir -p ~/.config/{i3,nvim,Code/User}
+# Link config files
+mkdir -p ~/.config/Code/User
 ln -s ~/code/dotfiles/code/keybindings.json ~/.config/Code/User/keybindings.json
 ln -s ~/code/dotfiles/code/settings.json ~/.config/Code/User/settings.json
+
+mkdir -p ~/.config/{i3,nvim}
 ln -s ~/code/dotfiles/i3/config ~/.config/i3/config
 ln -s ~/code/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
