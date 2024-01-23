2e97f6bf544a191d178c2412404e4cff1c1296a3
mhu <marcel.huismann@protonmail.ch>
2024-01-23 15:05:11 -0500

    * updated Code config location * updated polybar config

--- install.sh
+++ install.sh
@@ -13,20 +13,14 @@ chsh -s /bin/zsh
 ln -s ~/code/dotfiles/.zshrc ~/.zshrc
 
 # Link config files
-mkdir -p ~/.config/Code/User
-ln -s ~/code/dotfiles/code/keybindings.json ~/.config/Code/User/keybindings.json
-ln -s ~/code/dotfiles/code/settings.json ~/.config/Code/User/settings.json
+mkdir -p ~/.var/app/com.visualstudio.code/config/Code/User
+ln -s ~/code/dotfiles/code/keybindings.json ~/.var/app/com.visualstudio.code/config/Code/User/keybindings.json
+ln -s ~/code/dotfiles/code/settings.json ~/.var/app/com.visualstudio.code/config/Code/User/settings.json
 
 mkdir -p ~/.config/{i3,polybar}
 ln -s ~/code/dotfiles/i3/config ~/.config/i3/config
 ln -s ~/code/dotfiles/polybar/launch.sh ~/.config/polybar/launch.sh
 ln -s ~/code/dotfiles/polybar/config.ini ~/.config/polybar/config.ini
-
-# Install terraform according to https://developer.hashicorp.com/terraform/install?product_intent=terraform#Linux
-echo "Installing terraform"
-wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update && sudo apt install terraform
 
 # Next steps
 echo "Set the initial wallpaper with `feh --bg-center /path/to/the/wallpaper.png`"
