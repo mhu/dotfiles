16941f5a8aeb65cb647554aba8dd1716d4e61093
mhu <marcel.huismann@protonmail.ch>
2025-11-25 00:23:17 -0500

    added noconfirm option to install commands

--- install-arch.sh
+++ install-arch.sh
@@ -2,13 +2,15 @@ sudo pacman -Syu
 
 if ! command -v yay >/dev/null 2>&1
 then
+  echo "Yay not found. Installing."
   sudo pacman -S --needed https://aur.archlinux.org/yay.git
   cd yay/
   makepkg -si
   cd ../
 fi
 
-sudo pacman -S fzf git go helix i3 lazygit magic-wormhole stow tailscale tldr zoxide zsh
+sudo pacman -S fzf git go helix i3 lazygit magic-wormhole stow tailscale tldr zoxide zsh --noconfirm
+yay -S deezer --noconfirm
 
 stow .
 mkdir ~/code
