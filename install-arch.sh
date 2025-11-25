e70fe41214a35c88ff461adc7f992128c7e0d882
mhu <marcel.huismann@protonmail.ch>
2025-11-25 00:16:03 -0500

    add stow to Arch install script

--- install-arch.sh
+++ install-arch.sh
@@ -1,13 +1,14 @@
-# update pacman
 sudo pacman -Syu
 
-# install Yay
 if ! command -v yay >/dev/null 2>&1
 then
   sudo pacman -S --needed https://aur.archlinux.org/yay.git
   cd yay/
   makepkg -si
+  cd ../
 fi
 
-# install tools
 sudo pacman -S fzf git go helix i3 lazygit magic-wormhole stow tailscale tldr zoxide zsh
+
+stow .
+mkdir ~/code
